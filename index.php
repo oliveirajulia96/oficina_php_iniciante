986980a1670b2a98c016244907e78954ba89d207
juliaeira <juliaeira@hotmail.com>
2017-11-18 14:00:23 -0500

    meu primeiro commit.

--- index.php
+++ index.php
@@ -9,9 +9,111 @@
                         
         <?php
 
-            // Coloque seu código aqui!
-
-        ?>
-
+    /*$cor = "amarelo";
+    $cor = "vermelho";
+    echo $cor;*/
+      ?>
+      
+      <?php
+  /*$cor = "vermelho";
+      var_dump($cor);*/
+      $a = 3;
+      $b = 5;
+      
+     /*$resultado = $a + $b;
+      
+     $resultado = $a - $b;
+      echo $resultado;
+   
+      $a = 5;
+      $b = 2;
+      
+      if($a > $b) {
+        echo "a é maior que b";
+      } else if($a == $b) {
+        echo "a é igual a b";
+      } else {
+        echo "a é menor que b";
+      }
+     
+      $i = 1;
+        while($i < 10) {
+          $i = $i + 1;
+          echo $i . "<br>";
+        }*/
+          
+      /*$i = 1;
+      
+        while($i < 10) {
+          $i = $i + 1;
+          
+          if ($i%2 == 0) {
+            echo "O valor de $i é par<br>";
+          } else {
+          echo "O valor de $i é ímpar<br>";
+          }
+        }*/
+      /*for($i = 1; $i < 10; $i++) {
+        echo "O valor de i é: $i<br>";
+      }*/
+      
+     /* $nome = "Julia";
+      
+      switch ($nome) {
+        case "Ana": 
+          echo "olá Ana";
+          break;
+          
+        case "Pedro":
+          echo "olá Pedro";
+          break; 
+          
+        default: 
+          echo "Não sei quem você é";
+          
+      }*/
+      
+     /* function soma($a, $b)  {
+        $resultado = $a + $b;
+        return $resultado; 
+      }
+      
+      $numero1 = 23;
+      $numero2 = 20;
+      
+      $resultadoFuncao = soma($numero1, $numero2);
+      echo $resultadoFuncao;*/
+        
+      /*$cores = ["vermelho", "azul"];
+      echo $cores[1];*/
+      
+      /*$pessoa = ["nome" => "Ana", "Idade" => 23];
+      $pessoa*/
+      
+      /*$numeros = [1, 2, 3];
+      array_push($numeros, 4);
+      echo $numeros[3];*/
+      
+      /*$frutas = array("maça", "laranja", "morango");
+      
+      foreach($frutas as $fruta) {
+        echo "A fruta é $fruta<br>";
+      }*/
+      
+      $comidas = array("batata", "pastel", "melancia");
+      foreach($comidas as $comida) {
+        echo "a melancia é $comida<br>";
+        
+      }
+      array_pop($comidas);
+      foreach($comidas as $comida) {
+        echo "a melancia é $comida<br>";
+      }
+      
+      
+      
+   ?>
+      
+      
     </body>
 </html>
